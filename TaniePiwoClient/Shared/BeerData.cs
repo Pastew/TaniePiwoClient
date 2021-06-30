e19cc2d26bf8b89fed153274a7e329822ee15c8a
kamil.p <kamil.p@yggdrasilgaming.com>
2021-06-30 15:58:22 +0200

    Getting back to work. Added unrelevant 2 classes

---
+++ TaniePiwoClient/Shared/BeerData.cs
@@ -0,0 +1,8 @@
+namespace TaniePiwoClient.Shared
+{
+    public class BeerData
+    {
+        public string Name;
+        public int Price; // Cena w groszach, np. "650" = 6.50zł
+    }
+}
---
+++ TaniePiwoClient/Shared/PubData.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace TaniePiwoClient.Shared
+{
+    public class PubData
+    {
+        public string Name;
+        public float Lat;
+        public float Lng;
+        public List<BeerData> BeerDataList;
+    }
+}
